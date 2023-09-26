343f7177b92cbc4fa17b2024970581d0aca582b3
mrkamin <108926875+mrkamin@users.noreply.github.com>
2023-09-27 02:59:51 +0500

    Add Hero component

---
+++ src/components/Hero.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div className="hero-container">
+      <h1>Fun Facts About React</h1>
+      <ul>
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100K stars on GitHub</li>
+        <li>Is maintained by Facebook</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
+    </div>
+  );
+};
+export default Hero;
