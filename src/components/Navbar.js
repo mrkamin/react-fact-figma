9e343567ba30f8f7647580d97369177b25e98ac0
mrkamin <108926875+mrkamin@users.noreply.github.com>
2023-09-27 04:55:38 +0500

    Configure Linters

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,14 @@
-import React from "react";
-import IMG from "../assets/react-logo.png";
+import React from 'react';
+import IMG from '../assets/react-logo.png';
 
-const Navbar = () => {
-  return (
-    <header className="flex">
-      <div className="title-container flex">
-        <img src={IMG} alt="logo" />
-        <h2>ReactFact</h2>
-      </div>
-      <h3>React Course - Project 1</h3>
-    </header>
-  );
-};
+const Navbar = () => (
+  <header className="flex">
+    <div className="title-container flex">
+      <img src={IMG} alt="logo" />
+      <h2>ReactFact</h2>
+    </div>
+    <h3>React Course - Project 1</h3>
+  </header>
+);
 
 export default Navbar;
