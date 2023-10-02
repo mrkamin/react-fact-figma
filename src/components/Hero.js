90d8f51f19ae7e6204926503563bea379255ac1d
mrkamin <108926875+mrkamin@users.noreply.github.com>
2023-10-02 23:47:46 +0500

    Complete Hero component

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,32 +2,44 @@ import React from 'react';
 import { FiGithub, FiLinkedin } from 'react-icons/fi';
 
 const Hero = () => (
-  <div className="hero-container flex">
+  <div className="hero-container flex container">
     <h3>Mohammad Rafi Amin</h3>
     <h5>Full Stack Developer</h5>
-    <div className="social-btn d-flex gap-3 mt-3">
-      <div className="portfolio__item-cta">
-        <a
-          href="https://github.com/mrkamin"
-          className="btn btn-secondary"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <FiGithub />
-          {' '}
-          GITHUB
-        </a>
-        <a
-          href="https://www.linkedin.com/in/mohammad-rafi-amin/"
-          className="btn btn-primary"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <FiLinkedin />
-          {' '}
-          LINKEDIN
-        </a>
-      </div>
+    <div className="social-btn d-flex gap-3 mt-3 container">
+      <a
+        href="https://github.com/mrkamin"
+        className="btn btn-secondary d-flex align-items-center justify-content-center gap-2"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FiGithub />
+        {' '}
+        Github
+      </a>
+      <a
+        href="https://www.linkedin.com/in/mohammad-rafi-amin/"
+        className="btn btn-primary d-flex align-items-center justify-content-center gap-2"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FiLinkedin />
+        {' '}
+        Linkedin
+      </a>
+    </div>
+    <div className="about container mt-2">
+      <h4>About</h4>
+      <p className="about-p fs-7">
+        Full-stack developer passionate about crafting applications in Ruby,
+        React, and JavaScript.
+      </p>
+    </div>
+    <div className="about container">
+      <h4>Intersted</h4>
+      <p className="about-p fs-7">
+        web development, front-end technologies, personal projects, learning,
+        problem-solving, and teamwork.
+      </p>
     </div>
   </div>
 );
