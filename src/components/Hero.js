72cc556e7490ebd7bdf8ff7565a44376ac353481
mrkamin <108926875+mrkamin@users.noreply.github.com>
2023-10-10 04:20:04 +0500

    compleate hero section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,25 +1,13 @@
 import React from 'react';
-import IMG1 from '../assets/image 22.png';
-import IMG2 from '../assets/image 23.png';
-import IMG3 from '../assets/image 24.png';
-import IMG4 from '../assets/image 25.png';
-import IMG5 from '../assets/image 26.png';
-import IMG6 from '../assets/image 27.png';
-import IMG7 from '../assets/image 28.png';
-import IMG8 from '../assets/image 29.png';
-import IMG9 from '../assets/image 30.png';
+import IMG from '../assets/Group 77.png';
 
 const Hero = () => (
-  <div className="hero-container container">
-    <img src={IMG7} alt="IMG1" />
-    <img src={IMG9} alt="IMG1" />
-    <img src={IMG5} alt="IMG1" />
-    <img src={IMG6} alt="IMG1" />
-    <img src={IMG8} alt="IMG1" />
-    <img src={IMG1} alt="IMG1" />
-    <img src={IMG2} alt="IMG1" />
-    <img src={IMG4} alt="IMG1" />
-    <img src={IMG3} alt="IMG1" />
+  <div className="hero-container container d-flex mt-2 flex-column">
+    <img src={IMG} alt="IMG" />
+    <h1>Online Experiences</h1>
+    <p>
+      Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.
+    </p>
   </div>
 );
 export default Hero;
